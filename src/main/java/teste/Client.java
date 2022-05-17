d8ee786482c6a98f14cbcce7b8d7824a389f22ff
Flávio Freitas Pinter <f@pinter.dev>
2022-05-17 20:09:47 -0300

    Versão preliminar do menu de interação criada

--- src/main/java/teste/Client.java
+++ src/main/java/teste/Client.java
@@ -2,6 +2,7 @@ package teste;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private Socket socket;
@@ -26,26 +27,63 @@ public class Client {
         socket.close();
     }
 
-    public void sendFile(String path){
+    public void sendFile(String path) throws FileNotFoundException {
         File file = new File(path);
+        OutputStream os = new FileOutputStream(file);
         out.println(file);
     }
 
     public void receiveFile() throws IOException {
-        in = new BufferedReader(new FileReader(in.readLine()));
-        String strCurrentLine;
-        while ((strCurrentLine = in.readLine()) != null) {
-            System.out.println(strCurrentLine);
-        }
+        FileInputStream fis = new FileInputStream(in.readLine());
+        FileOutputStream fos = new FileOutputStream("C:\\temp\\TESTEEEEE.pdf");
+        byte[] bytes = new byte[1024];
+        fis.read(bytes, 0, bytes.length);
+        fos.write(bytes, 0, bytes.length);
+//        in = new BufferedReader(new FileReader(in.readLine()));
+//        String strCurrentLine;
+//        while ((strCurrentLine = in.readLine()) != null) {
+//            System.out.println(strCurrentLine);
+//        }
     }
 
     public static void main(String[] args) throws IOException {
         Client c = new Client();
-        c.startConnection("localhost", 12345);
-//        c.receiveFile();
-        c.sendFile("D:\\temp\\testSV.csv");
+//        c.startConnection("localhost", 12345);
+//        c.sendFile("D:\\temp\\TESTEEEEE.pdf");
 //        System.out.println(c.sendMessage("OII"));
 //        System.out.println(c.sendMessage("hello server"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo(a)! O que deseja fazer?");
+        System.out.println("1: Trocar mensagens.");
+        System.out.println("2: Trocar arquivos.");
+        System.out.print("Digite um número: ");
+        int option = sc.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Servidor iniciando, esperando conexão...");
+                c.startConnection("localhost", 12345);
+                break;
+            case 2:
+                System.out.println("Gostaria de enviar ou receber arquivos?");
+                System.out.println("1: Enviar.");
+                System.out.println("2: Receber.");
+                System.out.println("Digite um número para escolher: ");
+                int fileOption = sc.nextInt();
+                switch (fileOption) {
+                    case 1:
+                        System.out.println("Cole o caminho do arquivo: ");
+                        String path = sc.next();
+                        System.out.println("Servidor iniciando!");
+                        System.out.println("Esperando cliente se conectar...");
+                        c.startConnection("localhost", 12345);
+                        c.sendFile(path);
+                    case 2:
+                        System.out.println("Servidor iniciando!");
+                        System.out.println("Esperando o cliente se conectar...");
+                        c.startConnection("localhost", 12345);
+                        c.receiveFile();
+                }
+        }
     }
 }
 
--- src/main/java/teste/Server.java
+++ src/main/java/teste/Server.java
@@ -3,6 +3,7 @@ package teste;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     private Socket clientSocket;
@@ -32,7 +33,7 @@ public class Server {
 
     public void receiveFile() throws IOException {
         FileInputStream fis = new FileInputStream(in.readLine());
-        FileOutputStream fos = new FileOutputStream("C:\\temp\\tttttttttttttt.csv");
+        FileOutputStream fos = new FileOutputStream("C:\\temp\\testSV.csv");
         byte[] bytes = new byte[1024];
         fis.read(bytes, 0, bytes.length);
         fos.write(bytes, 0, bytes.length);
@@ -57,11 +58,39 @@ public class Server {
 
     public static void main(String[] args) {
         try {
-            Server sv = new Server();
-            sv.start(12345);
 //            sv.sendFile("D:\\temp\\TESTEEEEE.pdf");
-            sv.receiveFile();
-//            System.out.println(sv.sendMessage("AI"));
+            Server sv = new Server();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Bem vindo(a)! O que deseja fazer?");
+            System.out.println("1: Trocar mensagens.");
+            System.out.println("2: Trocar arquivos.");
+            System.out.print("Digite um número: ");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Servidor iniciando, esperando o cliente se conectar...");
+                    sv.start(12345);
+                    break;
+                case 2:
+                    System.out.println("Gostaria de enviar ou receber arquivos?");
+                    System.out.println("1: Enviar.");
+                    System.out.println("2: Receber.");
+                    System.out.println("Digite um número para escolher: ");
+                    int fileOption = sc.nextInt();
+                    switch (fileOption){
+                        case 1:
+                            System.out.println("Cole o caminho do arquivo: ");
+                            String path = sc.next();
+                            System.out.println("Servidor iniciando, esperando o cliente se conectar...");
+                            sv.start(12345);
+                            sv.sendFile(path);
+                        case 2:
+                            System.out.println("Servidor iniciando, esperando o cliente se conectar...");
+                            sv.start(12345);
+                            sv.receiveFile();
+                    }
+                    break;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
